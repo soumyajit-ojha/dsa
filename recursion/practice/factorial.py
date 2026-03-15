8d9ab455589379a07b831bf6dfb3f0258dcb0e0a
soumyajit-ojha <soumyajitojhag2@gmail.com>
2026-03-15 10:54:16 +0530

    removed all files.

--- recursion/practice/factorial.py
+++
@@ -1,7 +0,0 @@
-def factorial(n: int) -> int:
-    if n <= 1:
-        return 1
-    return factorial(n-1) * n
-
-res = factorial(5)
-print(res)
--- recursion/practice/sum_n_nums.py
+++
@@ -1,8 +0,0 @@
-
-def sum_numbers(n: int) -> int:
-    # base condition
-    if n <= 0:
-        return 0
-    return sum_numbers(n-1) + n
-
-print(sum_numbers(5))
